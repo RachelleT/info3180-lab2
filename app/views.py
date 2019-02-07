59296aa8633652349d3d45f792e529bde3d02155
RachelleT <rache_trotman@hotmail.com>
2019-02-06 19:11:02 -0500

    Lab 2

--- app/views.py
+++ app/views.py
@@ -7,7 +7,7 @@ This file creates your application.
 
 from app import app
 from flask import render_template, request, redirect, url_for, flash
-
+import datetime
 
 ###
 # Routing for your application.
@@ -24,10 +24,18 @@ def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
 
-
+@app.route('/profile')
+def profile():
+	"""Render the website about profile page."""
+	return render_template('profile.html', datejoined=format_date_joined(2, 2019))
 ###
 # The functions below should be applicable to all Flask apps.
 ###
+
+def format_date_joined(month, year):
+	date_joined = datetime.date(year, month,7) # a specific date
+	## Format the date to return only month and year date
+	return "Joined "+ date_joined.strftime("%b, %Y")
 
 @app.route('/<file_name>.txt')
 def send_text_file(file_name):
